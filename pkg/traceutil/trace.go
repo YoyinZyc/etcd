c811d6ac7775a8465aee24ac032605aab7dcb784
yoyinzyc <yoyinzyc@gmail.com>
2019-09-10 11:14:53 -0700

    pkg: add log in trace.go tp dump all steps in the Trace.

--- pkg/traceutil/trace.go
+++ pkg/traceutil/trace.go
@@ -1,7 +1,16 @@
 package traceutil
 
 import (
+	"bytes"
+	"fmt"
 	"time"
+
+	"github.com/coreos/pkg/capnslog"
+	"go.uber.org/zap"
+)
+
+var (
+	plog = capnslog.NewPackageLogger("go.etcd.io/etcd", "trace")
 )
 
 type Trace struct {
@@ -21,4 +30,31 @@ func New(op string) *Trace {
 
 func (t *Trace) AddStep(msg string) {
 	t.steps = append(t.steps, step{time: time.Now(), msg: msg})
+}
+
+// Dump all steps in the Trace
+func (t *Trace) Log(lg *zap.Logger) {
+
+	var buf bytes.Buffer
+
+	buf.WriteString(fmt.Sprintf("The tracing of %v request:\n", t.operation))
+
+	buf.WriteString("Request started at:")
+	buf.WriteString(t.startTime.Format("2006-01-02 15:04:05"))
+	buf.WriteString(fmt.Sprintf(".%06d", t.startTime.Nanosecond()/1000))
+	buf.WriteString("\n")
+	lastStepTime := t.startTime
+	for i, step := range t.steps {
+		buf.WriteString(fmt.Sprintf("Step %d: %v Time cost: %v\n", i, step.msg, step.time.Sub(lastStepTime)))
+		//fmt.Println(step.msg, " costs: ", step.time.Sub(lastStepTime))
+		lastStepTime = step.time
+	}
+	buf.WriteString("Trace End\n")
+
+	s := buf.String()
+	if lg != nil {
+		lg.Info(s)
+	} else {
+		plog.Info(s)
+	}
 }
